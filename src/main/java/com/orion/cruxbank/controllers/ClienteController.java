37ef3b97f46e8e4cc202eb722d3359e942c3de9a
FrancoBrasil <fcbs.web@gmail.com>
2021-01-13 20:30:52 -0300

    Implementando ordenacao

--- src/main/java/com/orion/cruxbank/controllers/ClienteController.java
+++ src/main/java/com/orion/cruxbank/controllers/ClienteController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,9 +39,9 @@ public class ClienteController {
 
 	@GetMapping
 	public Page<ClienteDTO> lista(@RequestParam(required = false) String nome,
-			@RequestParam int pagina, @RequestParam int qtde) {
+			@RequestParam int pagina, @RequestParam int qtde, @RequestParam String ordenacao) {
 
-		Pageable paginacao = PageRequest.of(pagina, qtde);
+		Pageable paginacao = PageRequest.of(pagina, qtde, Direction.ASC, ordenacao);
 
 		if (nome == null) {
 			Page<Cliente> clientes = clienteRepository.findAll(paginacao);
