17dba18825dae097b7beec3c0261827aacbaeeac
FrancoBrasil <fcbs.web@gmail.com>
2021-01-14 10:02:43 -0300

    Autenticacao Concluída

--- src/main/java/com/orion/cruxbank/security/SecurityConfiguration.java
+++ src/main/java/com/orion/cruxbank/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.orion.cruxbank.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,17 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private AutenticacaoService service;
 	
 	// Config autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	// Config autorização
@@ -25,7 +28,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/clientes").permitAll()
-		.antMatchers(HttpMethod.GET, "/clientes/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/clientes/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	
 	// Config de recursos estáticos (req para css js e imagens)
